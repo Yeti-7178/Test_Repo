e0c2cb5733946b4a26e2550f5119a956275344fb
isvEnjoyer <nungarten@gmail.com>
2024-10-22 18:46:43 -0400

    Fixed up chassis

--- test/src/main/java/frc/robot/subsystems/TankDrive.java
+++ test/src/main/java/frc/robot/subsystems/TankDrive.java
@@ -2,31 +2,61 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel;
-
-import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController; // motor manager we should use instead of other previous things. very useful
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class TankDrive extends SubsystemBase {
-    PWMSparkMax m_Right1 = new PWMSparkMax(Constants.OperatorConstants.kRightFrontPort);
-    PWMSparkMax m_Left1 = new PWMSparkMax(Constants.OperatorConstants.kLeftFrontPort);
-    PWMSparkMax m_Right2 = new PWMSparkMax(Constants.OperatorConstants.kRightRearPort);
-    PWMSparkMax m_Left2 = new PWMSparkMax(Constants.OperatorConstants.kLeftRearPort);
+    CANSparkMax m_Right1 = new CANSparkMax(Constants.OperatorConstants.kRightFrontPort , CANSparkLowLevel.MotorType.kBrushless );
+    CANSparkMax m_Left1 = new CANSparkMax(Constants.OperatorConstants.kLeftFrontPort , CANSparkLowLevel.MotorType.kBrushless );
+    CANSparkMax m_Right2 = new CANSparkMax(Constants.OperatorConstants.kRightRearPort , CANSparkLowLevel.MotorType.kBrushless );
+    CANSparkMax m_Left2 = new CANSparkMax(Constants.OperatorConstants.kLeftRearPort , CANSparkLowLevel.MotorType.kBrushless );
+
+    RelativeEncoder m_EncoderRight1 = m_Right1.getEncoder();
+    RelativeEncoder m_EncoderLeft1 = m_Left1.getEncoder();
+
+    double gearboxRatio = 8.46;
+    double wheelRadius = 6;
+    double brakingForce = 70;
+
+    public double getInches( double rotations ) {
+        return rotations * 2 * Math.PI * (wheelRadius / gearboxRatio);
+    }
+
+    public double leftInches() {
+        return getInches(m_EncoderLeft1.getPosition());
+    }
+
+    public double rightInches() {
+        return getInches(m_EncoderRight1.getPosition());
+    }
+
+    public double getHeadingDeg() {
+        return ( leftInches() - rightInches() ) * ( 360 / 21.5 );
+    }
+
+    public double getDistanceTraveled() { 
+        return ( leftInches() + rightInches() ) / 2;
+    }
+
+    public double getVelocityInPerSec() {
+        double averageRPM = (m_EncoderLeft1.getVelocity() + m_EncoderRight1.getVelocity()) / 2;
+        return getInches(averageRPM) / 60;
+    }
+
+    public double getBrakingDistance() {
+        double velocity = getVelocityInPerSec();
+        double brakingTime = velocity / brakingForce;
+        return ( (brakingForce) * ( Math.pow(brakingTime, 2) ) + (velocity * brakingTime) );
+
+    }
 
     DifferentialDrive m_drive = new DifferentialDrive(m_Left1, m_Right1);
     public TankDrive() {
-        m_Right1.addFollower(m_Right2);
-        m_Left1.addFollower(m_Left2);
+        m_Right1.follow(m_Right2);
+        m_Left1.follow(m_Left2);
 
         m_Right1.restoreFactoryDefaults();
         m_Right2.restoreFactoryDefaults();
@@ -41,13 +71,12 @@ public class TankDrive extends SubsystemBase {
         m_Right1.setIdleMode(IdleMode.kBrake);
         m_Left1.setIdleMode(IdleMode.kBrake);
         m_Right2.setIdleMode(IdleMode.kBrake);
-        m_Left1.setIdleMode(IdleMode.kBrake);
 
         m_Right1.setInverted(true);
         m_Right2.setInverted(true);
     }
-    public void drive(double xSpeed, double zRotation, boolean b){
-        m_drive.tankDrive(xSpeed, -zRotation, b);
+    public void drive(double leftSpeed, double rightSpeed, boolean b){
+        m_drive.tankDrive( leftSpeed , rightSpeed );
     }
 
     public void setBrakeMode(){
