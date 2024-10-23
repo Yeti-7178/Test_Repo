479a27cfbff8c4ed3bd087b000f305e13792f05e
FRC-7178 <7178robotics@gmail.com>
2024-10-23 09:24:48 -0400

    Fixed some errors adn reorganized

--- test/src/main/java/frc/robot/Constants.java
+++ test/src/main/java/frc/robot/Constants.java
@@ -15,10 +15,7 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
-    public static int kLeftRearPort = 234567;
-    public static int kRightRearPort = 6543;
-    public static int kLeftFrontPort = 41682;
-    public static int kRightFrontPort = 522752475;
+    
 
     public static final int kCurrentLimit = 40;
 
@@ -27,5 +24,11 @@ public final class Constants {
   public static final class OIConstants {
     public static final int controller1 = 0;
     public static final int controller2 = 1;
+  }
+  public static final class TankConstants{
+    public static int kLeftRearPort = 234567;
+    public static int kRightRearPort = 6543;
+    public static int kLeftFrontPort = 41682;
+    public static int kRightFrontPort = 522752475;
   }
 }
--- test/src/main/java/frc/robot/subsystems/TankDrive.java
+++ test/src/main/java/frc/robot/subsystems/TankDrive.java
@@ -2,7 +2,8 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkLowLevel;
 
 import edu.wpi.first.wpilibj.PWM;
@@ -15,13 +16,53 @@ import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+import frc.robot.Constants.*;
 
 public class TankDrive extends SubsystemBase {
-    PWMSparkMax m_Right1 = new PWMSparkMax(Constants.OperatorConstants.kRightFrontPort);
-    PWMSparkMax m_Left1 = new PWMSparkMax(Constants.OperatorConstants.kLeftFrontPort);
-    PWMSparkMax m_Right2 = new PWMSparkMax(Constants.OperatorConstants.kRightRearPort);
-    PWMSparkMax m_Left2 = new PWMSparkMax(Constants.OperatorConstants.kLeftRearPort);
+    CANSparkMax m_Right1 = new CANSparkMax(TankConstants.kRightFrontPort , MotorType.kBrushless );
+    CANSparkMax m_Left1 = new CANSparkMax(TankConstants.kLeftFrontPort , MotorType.kBrushless );
+    CANSparkMax m_Right2 = new CANSparkMax(TankConstants.kRightRearPort , MotorType.kBrushless );
+    CANSparkMax m_Left2 = new CANSparkMax(TankConstants.kLeftRearPort , MotorType.kBrushless );
+
+    RelativeEncoder m_EncoderRight1 = m_Right1.getEncoder();
+    RelativeEncoder m_EncoderLeft1 = m_Left1.getEncoder();
+
+    double gearboxRatio = 8.46;
+    double wheelRadius = 6;
+    double brakingForce = 70;
+
+    public double getInches( double rotations ) {
+        return rotations * 2 * Math.PI * (wheelRadius / gearboxRatio);
+    }
+
+    public double leftInches() {
+        return getInches(m_EncoderLeft1.getPosition());
+    }
+
+    public double rightInches() {
+        return getInches(m_EncoderRight1.getPosition());
+    }
+
+    public double getHeadingDeg() {
+        return ( leftInches() - rightInches() ) * ( 360 / 21.5 );
+    }
+
+    public double getDistanceTraveled() { 
+        return ( leftInches() + rightInches() ) / 2;
+    }
+
+    public double getVelocityInPerSec() {
+        double averageRPM = (m_EncoderLeft1.getVelocity() + m_EncoderRight1.getVelocity()) / 2;
+        return getInches(averageRPM) / 60;
+    }
+
+    public double getBrakingDistance() {
+        double velocity = getVelocityInPerSec();
+        double brakingTime = velocity / brakingForce;
+        return ( (brakingForce) * ( Math.pow(brakingTime, 2) ) + (velocity * brakingTime) );
+
+    }
+
 
     DifferentialDrive m_drive = new DifferentialDrive(m_Left1, m_Right1);
     public TankDrive() {
@@ -33,10 +74,10 @@ public class TankDrive extends SubsystemBase {
         m_Left1.restoreFactoryDefaults();
         m_Left2.restoreFactoryDefaults();
 
-        m_Right1.setSmartCurrentLimit(Constants.OperatorConstants.kCurrentLimit);
-        m_Right2.setSmartCurrentLimit(Constants.OperatorConstants.kCurrentLimit);
-        m_Left1.setSmartCurrentLimit(Constants.OperatorConstants.kCurrentLimit);
-        m_Left2.setSmartCurrentLimit(Constants.OperatorConstants.kCurrentLimit);
+        m_Right1.setSmartCurrentLimit(OperatorConstants.kCurrentLimit);
+        m_Right2.setSmartCurrentLimit(OperatorConstants.kCurrentLimit);
+        m_Left1.setSmartCurrentLimit(OperatorConstants.kCurrentLimit);
+        m_Left2.setSmartCurrentLimit(OperatorConstants.kCurrentLimit);
 
         m_Right1.setIdleMode(IdleMode.kBrake);
         m_Left1.setIdleMode(IdleMode.kBrake);
@@ -46,8 +87,9 @@ public class TankDrive extends SubsystemBase {
         m_Right1.setInverted(true);
         m_Right2.setInverted(true);
     }
-    public void drive(double xSpeed, double zRotation, boolean b){
-        m_drive.tankDrive(xSpeed, -zRotation, b);
+
+    public void drive(double leftSpeed, double rightSpeed, boolean b){
+        m_drive.tankDrive( leftSpeed , rightSpeed, b);
     }
 
     public void setBrakeMode(){
